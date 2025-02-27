1599bd653133abd942c4d51f67f2dfae59355802
xFru1T <max.shibaefff@gmail.com>
2025-02-28 02:41:47 +0300

    Result

--- Main.java
+++ Main.java
@@ -1,8 +1,8 @@
 import java.util.Scanner;
-class Long1 {
+class Float {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Print float number for checking on positive/negative/zero: ");
+        System.out.print("Print float number for checking on positive/negative/zero: ");
         float num = scanner.nextFloat();
         if (num == 0) {
             System.out.println("is number zero");
